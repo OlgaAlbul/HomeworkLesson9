8bb627d26834631e2f27f60f4481cbcbe57c9e1b
AlbulOlya <albul.olya@gmail.com>
2023-02-27 20:41:39 +0500

    Task 2 solved

---
+++ Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+int ReadInt(string text)
+{
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int PrintSum1( int m, int n)
+{
+    int sum = n;
+    if (m!=n)
+    {
+       sum = sum + PrintSum1(m, n+1);
+    }
+    return sum;
+}
+int PrintSum2( int m, int n)
+{
+    int sum = m;
+    if (m!=n)
+    {
+       sum = sum + PrintSum2(m+1, n); 
+    }
+    return sum;
+}
+
+int numberM = ReadInt("Enter number M: ");
+int numberN = ReadInt("Enter number N: ");
+
+if (numberM>numberN)
+{
+    Console.WriteLine($"The sum of the numbers from {numberM} to {numberN} is equal to: {PrintSum1(numberM,numberN)}");
+}
+else
+Console.WriteLine($"The sum of the numbers from {numberM} to {numberN} is equal to: {PrintSum2(numberM,numberN)}");
