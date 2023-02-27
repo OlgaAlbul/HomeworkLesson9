6a7191fa4cef258c076841606f3d0b5379550b5b
AlbulOlya <albul.olya@gmail.com>
2023-02-27 20:13:26 +0500

    Task 1 solved

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+int ReadInt(string text)
+{
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void PrintNumbers1(int M, int N)
+{
+    Console.Write($"{M}, ");
+    if(M==N)
+    Console.WriteLine("end");
+    else PrintNumbers1(M-1,N);    
+}
+
+void PrintNumbers2 (int M, int N)
+{
+    if (N!=0)
+    {
+        PrintNumbers2( M, N-1);
+        Console.Write($"{N}, ");
+    }
+}
+
+int numberM = ReadInt("Enter number M: ");
+int numberN = ReadInt("Enter number N: ");
+
+if (numberM>numberN)
+{
+    PrintNumbers1(numberM, numberN);
+}
+else PrintNumbers2(numberM, numberN);
