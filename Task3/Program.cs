e15e8fd3c8eaac0e35dcb9541b34a93216bb9d28
AlbulOlya <albul.olya@gmail.com>
2023-02-27 20:55:21 +0500

    Task 3 solved

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму цифр числа
+int ReadInt(string text)
+{
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SumNumbers (int n)
+{
+    if (n<1) return 0;
+    else return SumNumbers(n/10)+n%10;
+}
+
+int number = ReadInt("Enter the number: ");
+Console.WriteLine($"The sum of the digits of the {number} is {SumNumbers(number)}");
